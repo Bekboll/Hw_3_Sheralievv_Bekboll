8481ca510d75439ce90f8ae8e795a21c92b2a280
Bekboll <champcharacter@gmail.com>
2022-05-10 15:17:26 +0600

    Initial commit

---
+++ src/LimitException.java
@@ -0,0 +1,17 @@
+public class LimitException extends Throwable {
+    private String massage;
+    private Double remainingAmount;
+
+    public LimitException(String massage, Double remainingAmount) {
+        this.massage = massage;
+        this.remainingAmount = remainingAmount;
+    }
+
+    public String getMassage() {
+        return massage;
+    }
+
+    public Double getRemainingAmount() {
+        return remainingAmount;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount=new BankAccount();
+        bankAccount.depositSum(20000);
+        while (true){
+
+            try {
+                bankAccount.withDraw(6000);
+            } catch (LimitException e) {
+                try {
+                    System.out.println(e.getMassage());
+                    bankAccount.withDraw(e.getRemainingAmount());
+                } catch (LimitException ex) {
+                }
+                break;
+            }
+        }
+    }
+}
